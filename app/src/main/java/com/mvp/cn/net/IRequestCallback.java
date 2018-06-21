572e72fb46986b1cf42d0b08bad10585511520f8
qiaohao <qiaohao0125>
2018-06-21 15:12:50 +0800

    增加网络请求响应判断

--- app/src/main/java/com/mvp/cn/net/IRequestCallback.java
+++ app/src/main/java/com/mvp/cn/net/IRequestCallback.java
@@ -1,12 +1,45 @@
 package com.mvp.cn.net;
 
+
+
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * 网络请求的回调接口
+ * 根据响应判断是否登录
  */
 
-public interface IRequestCallback {
+public abstract class IRequestCallback<T> implements Callback<T> {
 
-     void onSuccess(int reqesutCode, String response);
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+        if (response.raw().code() == 200) {
+            onSuccess(response.body());
+        }else{
+           onFailure("请求失败");
+        }
+    }
 
-     void onFailure(int requestCode, String responseBody);
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        if (t.getCause() instanceof ConnectException) {
+            onNetError("服务器连接失败");
+        }else if(t.getMessage()!=null&&t.getMessage().equals("timeout")){
+            onNetError("服务器连接超时");
+        }else{
+            onFailure(t.getMessage());
+        }
+    }
+
+
+    public abstract void onSuccess(T response);
+
+    public abstract void onNetError(String response);
+
+    public abstract void onFailure(String message);
 }
