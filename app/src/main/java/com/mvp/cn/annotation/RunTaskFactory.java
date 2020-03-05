f9a68c5c565ddbfe5f69a2a98d247fcfa94975d9
qiaohao <qiaohao@otasim.com>
2020-03-05 16:40:27 +0800

    RunTaskFactory代码优化

--- app/src/main/java/com/mvp/cn/annotation/RunTaskFactory.java
+++ app/src/main/java/com/mvp/cn/annotation/RunTaskFactory.java
@@ -1,12 +1,15 @@
 package com.mvp.cn.annotation;
 
-import android.util.Log;
+
+import com.tencent.mars.xlog.Log;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
 /**
  * Created by iqiao on 2020-01-09 11:04
  * Desc: 改工厂类是负责通过传入的type来执行不同的SwitchRunTaskManager中注解对应的方法的
+ *
  * @author iqiao
  */
 
@@ -15,7 +18,7 @@ public class RunTaskFactory {
     public static void buildRunTask(int type, String content) {
         try {
             //获取到类对象
-            Class<SwitchRunTaskManager> manager = (Class<SwitchRunTaskManager>) Class.forName("com.zq.project.test.SwitchRunTaskManager");
+            Class<SwitchRunTaskManager> manager = (Class<SwitchRunTaskManager>) Class.forName("com.mvp.cn.annotation.SwitchRunTaskManager");
             //获取该类下的所有方法
             Method[] methods = manager.getMethods();
             //遍历所有方法
@@ -24,10 +27,10 @@ public class RunTaskFactory {
                 //判断该方法上有无SwitchType注解
                 if (flag) {
                     SwitchType type1 = method.getAnnotation(SwitchType.class);
-                    if (type==type1.value()){
+                    if (type == type1.value()) {
                         //有注解，并且判断通过则执行该方法
-                        method.invoke(manager.newInstance(),content);
-                    }else{
+                        method.invoke(manager.newInstance(), content);
+                    } else {
                         Log.e("test", "使用了注解，但条件不符合");
                     }
                 } else {
